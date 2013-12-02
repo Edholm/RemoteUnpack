d0f0562d745d986a0ba4974279264b5443938aea
Emil Edholm <emil@edholm.it>
2013-12-03 00:39:54 +0100

    Protocol version bump and some prettier stdout

--- Server.py
+++ Server.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
-# This is the server application that listens for connections and
-# unpacks archives in its own thread.
-#
-# Copyright 2013 Emil Edholm <emil@edholm.it>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+""" This is the server application that listens for connections and
+unpacks archives in its own thread.
+
+Copyright 2013 Emil Edholm <emil@edholm.it>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 #import socket
 #import threading
@@ -25,7 +27,7 @@ import os
 import time
 
 # The protocol this server supports/manages.
-protocol = "RU/0.3"
+protocol = "RU/0.4"
 
 
 def _recvall(sock, n):
@@ -53,7 +55,8 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         addr, port = self.client_address
-        print("Connection from {}:{}".format(addr, port))
+        import datetime
+        print("Connection from {}:{} on {} {}".format(addr, port, datetime.datetime.now() , "{"))
         msgRaw = receive(self.request)
         msg = ''
         if msgRaw is not None:
@@ -62,7 +65,12 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
             print("{}:{} closed the connection".format(addr, port))
             return False
 
-        print("Received: {}".format(msg))
+        print("\tRequest: method {} over protocol {} from {}:{}".format(msg['method'], msg['protocol'], addr, port))
+
+        if not self.verify_protocol(msg['protocol']):
+            self.reply_with_code('506', 'Protocol Not Supported')
+            print("}")
+            return
 
         # Try calling the method requested
         try:
@@ -71,18 +79,13 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
             func(msg)
         except AttributeError:
             self.reply_with_code('501', 'Not Implemented')
+        print("}")
 
     def verify_protocol(self, client_protocol):
         """Verify we're using the same protocol."""
         if not protocol == client_protocol:
-            self.reply_with_code('506', 'Protocol Not Supported')
             return False
         return True
-
-    def handle_bepa(self, msg):
-        self.reply_with_code()
-        self.reply_with_code()
-        self.reply_with_code()
 
     def handle_get(self, msg):
         """ The GET method is equivalent to the *nix command ls.
@@ -90,20 +93,14 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
             The method will return the list in an arbitrarily order
 
         """
-        if not self.verify_protocol(msg['protocol']):
-            return
-
         if os.path.lexists(msg['path']):
-            print("Replying with directory contents")
-            dir_contents = "\n".join(os.listdir(msg['path']))
+            print("\tSending dir contents for " + msg['path'])
+            dir_contents = (os.listdir(msg['path']))
             self.reply_with_code(data=dir_contents)
         else:
             self.reply_with_code(404, "Not Found")
 
     def handle_unpack(self, msg):
-        if not self.verify_protocol(msg['protocol']):
-            return
-
         # Set client ready for receiveing progress output
         self.reply_with_code('202', 'Accepted')
         time.sleep(1)
@@ -118,6 +115,8 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         print("done")
 
     def reply_with_code(self, code='200', phrase='OK', data=''):
+        addr, port = self.client_address
+        print("\tReply: code {} - {} to {}:{}".format(code, phrase, addr, port))
         from struct import pack
         reply = json.dumps({
             'protocol': protocol,
